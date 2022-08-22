e4cea99c44da1aa79cd76ca8d2b547410257df4b
MexL <madiajijah7@gmail.com>
2022-08-22 18:51:05 +0800

    test

--- Dian_Rahmadani.js
+++ Dian_Rahmadani.js
@@ -1,30 +1,41 @@
 //no 1 display Ok when multiple by 3, and display yes when multiple by 4
 const displayOk = (word) => {
-	let result = "";
 	if (word % 3 === 0 && word % 4 === 0) {
-		result = "OKYES";
+		return "OKYES";
 	} else if (word % 3 === 0) {
-		result = "OK";
+		return "OK";
 	} else if (word % 4 === 0) {
-		result = "YES";
+		return "YES";
 	} else {
-		result = word;
+		return word;
 	}
-	return result;
 };
+console.log(displayOk(1));
+console.log(displayOk(2));
+console.log(displayOk(3));
+console.log(displayOk(4));
+console.log(displayOk(5));
+console.log(displayOk(6));
+console.log(displayOk(7));
+console.log(displayOk(8));
+console.log(displayOk(9));
+console.log(displayOk(10));
+console.log(displayOk(11));
+console.log(displayOk(12));
+console.log(displayOk(13));
+console.log(displayOk(14));
 console.log(displayOk(15));
 
 // no 2 isPalindrome
-// return true if string is palindrome without using upercase and lowercase
+// return true if string is palindrome without using reverse upercase and lowercase
 const isPalindrome = (word) => {
-	const words = word.length;
-	for (let i = 0; i < words / 2; i++) {
-		if (word[i].toUpperCase() !== word[words - 1 - i].toUpperCase()) {
-			return false;
-		}
+	let reverse = "";
+	for (let i = word.length - 1; i >= 0; i--) {
+		reverse += word[i];
 	}
-	return true;
+	return word.toLowerCase() === reverse.toLocaleLowerCase();
 };
+
 console.log(isPalindrome("maLam"));
 console.log(isPalindrome("Level"));
 console.log(isPalindrome("kasur ini ruSak"));
@@ -40,4 +51,5 @@ const maxSecondNumber = (arrays) => {
 	}
 	return max;
 };
+
 console.log(maxSecondNumber([12, 4, 8, 15, 67, 99, 3]));
