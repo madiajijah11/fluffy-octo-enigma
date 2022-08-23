4e24aac44c56b4dabfcacf92452cd0999f4871e8
MexL <madiajijah7@gmail.com>
2022-08-23 19:49:01 +0800

    add navigation bar front end

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme";
+import NavigationBar from "./components/Navbar";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<ChakraProvider resetCSS>
+			<NavigationBar />
+			<App />
+		</ChakraProvider>
+	</React.StrictMode>
+);
