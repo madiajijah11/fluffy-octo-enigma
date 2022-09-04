d8756b3625d7b08e5fd9ae6e449fd19f78500a75
MexL <madiajijah7@gmail.com>
2022-09-04 21:10:32 +0800

    update besar besaran

--- client/src/api/users/index.js
+++
@@ -1,5 +0,0 @@
-import fetcher from "../../lib/axiosInstance";
-
-export const userLogin = () => {
-	return fetcher.post("/auth/login");
-};
--- client/src/lib/sessionToken.js
+++
@@ -1,8 +0,0 @@
-// get token from sessionStorage
-
-const sessionToken = () => {
-	const user = JSON.parse(sessionStorage.getItem("user"));
-	return user?.token;
-};
-
-export default sessionToken;
